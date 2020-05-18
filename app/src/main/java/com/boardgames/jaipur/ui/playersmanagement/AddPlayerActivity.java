29a915195d5cb61f589726efbc7116d3a379e33a
Balaji Rajagopal <balabala1989@gmail.com>
2020-05-19 00:27:33 +0530

    Refactoring Profile pic picking activity

--- app/src/main/java/com/boardgames/jaipur/ui/playersmanagement/AddPlayerActivity.java
+++ app/src/main/java/com/boardgames/jaipur/ui/playersmanagement/AddPlayerActivity.java
@@ -2,17 +2,14 @@ package com.boardgames.jaipur.ui.playersmanagement;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 
 import com.boardgames.jaipur.R;
+import com.boardgames.jaipur.entities.Player;
+import com.boardgames.jaipur.ui.utils.ImagePickAndCrop;
 import com.boardgames.jaipur.utils.CheckForPermissionsState;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.yalantis.ucrop.UCrop;
 
-import android.app.AlertDialog;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
@@ -20,27 +17,21 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.provider.OpenableColumns;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
 
 public class AddPlayerActivity extends AppCompatActivity {
 
     public static final String ADD_PLAYER_REPLY = "com.boardgames.jaipur.ui.playersmanagement.addplayeractivity.REPLY";
-    public static final int PICK_PICTURE_FOR_PROFILE_FROM_CAMERA = 3113;
-    public static final int PICK_PICTURE_FOR_PROFILE_FROM_GALLERY = 7112;
-    private AlertDialog dialog;
-    private String imageFilename;
-    private final int MAX_CROP_SIZE = 100;
-    private boolean permissionsGranted = false;
+    private boolean isPermissionGranted = false;
+    private final int REQUEST_IMAGE = 100;
+    private Player newPlayer;
+    private String playerAvatarAbsolutePath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,53 +42,27 @@ public class AddPlayerActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(R.string.title_activity_add_player);
 
-        permissionsGranted = CheckForPermissionsState.requestStorageCameraPermissions(AddPlayerActivity.this);
-
         ImageView playerAvatarImageView = (ImageView) findViewById(R.id.playerAvatarImageView);
         playerAvatarImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!permissionsGranted) {
-                    Toast.makeText(AddPlayerActivity.this, "This app requires permissions for using this feature. Please provide permissions in Settings!!!!! ", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                ViewGroup viewGroup = findViewById(android.R.id.content);
-                View dialogView = LayoutInflater.from(AddPlayerActivity.this).inflate(R.layout.layout_images_grid_view, viewGroup, false);
-                AlertDialog.Builder builder = new AlertDialog.Builder(AddPlayerActivity.this);
-                builder.setTitle(getResources().getString(R.string.alertdialog_title));
-                builder.setView(dialogView);
-                dialog = builder.create();
+                isPermissionGranted = CheckForPermissionsState.requestStorageCameraPermissions(AddPlayerActivity.this);
+                if (isPermissionGranted)
+                    ImagePickAndCrop.showOptions(AddPlayerActivity.this,
+                            new ImagePickAndCrop.PickerOptionListener() {
+                        @Override
+                        public void onCameraSelected() {
+                            launchCameraIntent();
+                        }
 
-                ImageView cameraImageView = (ImageView) dialogView.findViewById(R.id.cameraImageView);
-                cameraImageView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        imageFilename = System.currentTimeMillis() + ".jpg";
-                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,getImageFile(imageFilename));
-                        startActivityForResult(takePictureIntent,PICK_PICTURE_FOR_PROFILE_FROM_CAMERA);
-                    }
-                });
-
-                ImageView galleryImageView = (ImageView) dialogView.findViewById(R.id.galleryImageView);
-                galleryImageView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        startActivityForResult(galleryIntent, PICK_PICTURE_FOR_PROFILE_FROM_GALLERY);
-                    }
-                });
-
-                Button cancelButton = (Button) dialogView.findViewById(R.id.cancelButton);
-                cancelButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
-
-                dialog.show();
-
+                        @Override
+                        public void onGallerySelected() {
+                            launchGalleryIntent();
+                        }
+                    },
+                            getString(R.string.alertdialog_title_add_profile_photo));
+                else
+                    CheckForPermissionsState.deniedPermission(AddPlayerActivity.this);
             }
         });
 
@@ -120,6 +85,7 @@ public class AddPlayerActivity extends AppCompatActivity {
             }
 
             case R.id.addPlayerSaveButton: {
+                newPlayer = new Player();
                 setResult(RESULT_FIRST_USER, replyTent);
                 finish();
                 return true;
@@ -131,68 +97,49 @@ public class AddPlayerActivity extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        dialog.dismiss();
         ImageView playerAvatarImageView = (ImageView) findViewById(R.id.playerAvatarImageView);
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_PICTURE_FOR_PROFILE_FROM_CAMERA && resultCode == RESULT_OK) {
-            if (data != null) {
-                Uri sourceUri = getImageFile(imageFilename);
-                cropImage(sourceUri);
+        if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+            Uri imageUri = data.getParcelableExtra("path");
+            if (imageUri != null) {
+                Glide.with(this).load(imageUri).into(playerAvatarImageView);
+                return;
             }
         }
-        else if (requestCode == PICK_PICTURE_FOR_PROFILE_FROM_GALLERY && resultCode == RESULT_OK) {
-            if (data != null) {
-               Uri sourceUri = data.getData();
-               cropImage(sourceUri);
-            }
-        }
-        else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
-            Uri imageUri = UCrop.getOutput(data);
-            Glide.with(AddPlayerActivity.this).load(imageUri)
-                    .diskCacheStrategy(DiskCacheStrategy.DATA)
-                    .skipMemoryCache(true)
-                    .into(playerAvatarImageView);
-        }
-    }
-
-    private Uri getImageFile(String fileName) {
-        File primaryStorageVolume = new File(getExternalFilesDir(null), "images");
-        if (!primaryStorageVolume.exists())
-            primaryStorageVolume.mkdir();
-        File imageFile = new File(primaryStorageVolume, fileName);
-        return FileProvider.getUriForFile(AddPlayerActivity.this, getPackageName() + ".provider", imageFile);
-    }
-
-    private void cropImage(Uri sourceUri) {
-        Uri destinationUri = Uri.fromFile(new File(getExternalFilesDir(null), queryName(getContentResolver(), sourceUri)));
-        UCrop.Options options = new UCrop.Options();
-        options.setCompressionQuality(80);
-        options.setToolbarColor(ContextCompat.getColor(this, R.color.colorAccent));
-        options.setStatusBarColor(ContextCompat.getColor(this, R.color.colorAccent));
-        options.setActiveControlsWidgetColor(ContextCompat.getColor(this, R.color.colorAccent));
-        options.withAspectRatio(1, 1);
-        options.withMaxResultSize(100, 100);
-        UCrop.of(sourceUri, destinationUri)
-                .withOptions(options)
-                .start(this);
-    }
-
-    private static String queryName(ContentResolver resolver, Uri uri) {
-        Cursor returnCursor =
-                resolver.query(uri, null, null, null, null);
-        assert returnCursor != null;
-        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-        returnCursor.moveToFirst();
-        String name = returnCursor.getString(nameIndex);
-        returnCursor.close();
-        return name;
+        Glide.with(this).load(R.drawable.default_player_avatar).into(playerAvatarImageView);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        permissionsGranted = CheckForPermissionsState.requestStorageCameraPermissions(AddPlayerActivity.this);
+        isPermissionGranted = CheckForPermissionsState.requestStorageCameraPermissions(AddPlayerActivity.this);
     }
 
+    private void launchCameraIntent() {
+        Intent intent = new Intent(AddPlayerActivity.this, ImagePickAndCrop.class);
+        intent.putExtra(ImagePickAndCrop.INTENT_IMAGE_PICKER_OPTION, ImagePickAndCrop.FROM_CAMERA);
 
+        // setting aspect ratio
+        intent.putExtra(ImagePickAndCrop.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickAndCrop.INTENT_ASPECT_RATIO_X, 1); // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickAndCrop.INTENT_ASPECT_RATIO_Y, 1);
+
+        // setting maximum bitmap width and height
+        intent.putExtra(ImagePickAndCrop.INTENT_SET_BITMAP_MAX_WIDTH_HEIGHT, true);
+        intent.putExtra(ImagePickAndCrop.INTENT_BITMAP_MAX_WIDTH, 1000);
+        intent.putExtra(ImagePickAndCrop.INTENT_BITMAP_MAX_HEIGHT, 1000);
+
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+    private void launchGalleryIntent() {
+        Intent intent = new Intent(AddPlayerActivity.this, ImagePickAndCrop.class);
+        intent.putExtra(ImagePickAndCrop.INTENT_IMAGE_PICKER_OPTION, ImagePickAndCrop.FROM_GALLERY);
+
+        // setting aspect ratio
+        intent.putExtra(ImagePickAndCrop.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickAndCrop.INTENT_ASPECT_RATIO_X, 1); // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickAndCrop.INTENT_ASPECT_RATIO_Y, 1);
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
 }
